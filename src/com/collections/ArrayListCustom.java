888640f0d9d69c009bc1fbe93435793f1ad3cedc
junaid <junaid.jp@gmail.com>
2015-11-01 11:14:21 -0600

    More Collections Framework

---
+++ src/com/collections/ArrayListCustom.java
@@ -0,0 +1,28 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayListCustom {
+
+public static void main(String[] args) {
+		
+		List<Student> arrayList = new ArrayList<Student>();
+		Student student1 = new Student("Duncan", "Collections");
+		arrayList.add(student1);
+		arrayList.add(new Student("Ahad","Spring MVC"));
+		arrayList.add(new Student("KrishnaVeni","WEB services"));
+		arrayList.add(new Student("Modi","Struts Framework"));
+		arrayList.add(new Student("Jpasha","JMS "));
+		//linkedList.add(null);
+		
+		
+		for(int i = 0 ; i < arrayList.size(); i ++ ) { 
+			System.out.println(arrayList.get(i).getName());
+		}
+	
+	
+}
+}
